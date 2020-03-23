248f1e61fddcbe8abdd58c722226decbabfbd7bf
Maxime Raverdy <maxime.raverdy@gmail.com>
2020-03-23 14:53:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rails Templates
+
+Quickly generate a rails app configuration
+using [Rails Templates](http://guides.rubyonrails.org/rails_application_templates.html).
+
+
+## Grape
+
+Get a minimal rails 4.1 app ready to be deployed on Heroku with Grape Api and debugging gems.
+
+```bash
+rails new \
+  --database postgresql \
+  --webpack \
+  -m https://raw.githubusercontent.com/maximerdy/rails-templates/master/grape.rb \
+  CHANGE_THIS_TO_YOUR_RAILS_APP_NAME
+```
---
+++ grape.rb
@@ -0,0 +1,112 @@
+run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
+
+# GEMFILE
+########################################
+run 'rm Gemfile'
+file 'Gemfile', <<-RUBY
+source 'https://rubygems.org'
+ruby '#{RUBY_VERSION}'
+
+gem 'pg', '~> 0.21'
+gem 'puma'
+gem 'rails', '#{Rails.version}'
+
+gem 'grape'
+gem 'grape-entity'
+gem 'grape_on_rails_routes'
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'pry-rails'
+
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'dotenv-rails'
+
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'rspec-grape'
+
+  gem 'solargraph'
+end
+
+group :test do
+  gem 'database_cleaner-active_record'
+end
+
+RUBY
+
+# Procfile
+########################################
+file 'Procfile', <<-YAML
+web: bundle exec puma -C config/puma.rb
+YAML
+
+# README
+########################################
+markdown_file_content = <<-MARKDOWN
+Rails app generated with [https://github.com/MaximeRDY/rails-templates](https://github.com/https://github.com/MaximeRDY/rails-templates), created by the [MaximeRDY](https://github.com/MaximeRDY) team.
+MARKDOWN
+file 'README.md', markdown_file_content, force: true
+
+# Generators
+########################################
+generators = <<-RUBY
+config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework  :test_unit, fixture: false
+    end
+RUBY
+
+environment generators
+
+########################################
+# AFTER BUNDLE
+########################################
+after_bundle do
+  # Generators: db + simple form + pages controller
+  ########################################
+  rails_command 'db:drop db:create db:migrate'
+
+  # Git ignore
+  ########################################
+  append_file '.gitignore', <<-TXT
+
+# Ignore .env file containing credentials.
+.env*
+
+# Ignore Mac and Linux file system files
+*.swp
+.DS_Store
+TXT
+
+  # Dotenv
+  ########################################
+  run 'touch .env'
+
+  # Rubocop
+  ########################################
+  run 'curl -L https://raw.githubusercontent.com/MaximeRDY/rails-templates/master/.rubocop.yml > .rubocop.yml'
+
+  # Test
+  ########################################
+  run 'rm ./spec/rails_helper.rb'
+  run 'curl -L https://raw.githubusercontent.com/MaximeRDY/rails-templates/master/rails_helper.rb > .rubocop.yml'
+
+
+  # Git
+  ########################################
+  git :init
+  git add: '.'
+  git commit: "-m 'Initial commit with grape template from https://github.com/MaximeRDY/rails-templates'"
+
+  # Fix puma config
+  gsub_file('config/puma.rb', 'pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }', '# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }')
+end
